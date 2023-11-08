ef989da1d144075ec40f7e6f972c569f0d796bff
Jack Greer <jackxgreer@icloud.com>
2023-11-07 17:39:21 -0800

    helllp

---
+++ 2023Swerve/src/main/java/frc/robot/subsystems/IntakeSub.java
@@ -0,0 +1,54 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
+
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.motorcontrol.Talon;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class IntakeSub extends SubsystemBase {
+  /** Creates a new ExampleSubsystem. */
+
+  private final TalonFX intakerollers = new TalonFX(1);
+
+  public IntakeSub() {}
+
+  /**
+   * Example command factory method.
+   *
+   * @return a command
+   */
+  public int getrollertics() {
+    return 0;
+    
+    // Inline construction of command goes here.
+    // Subsystem::RunOnce implicitly requires `this` subsystem.
+
+  }
+
+  /**
+   * An example method querying a boolean state of the subsystem (for example, a digital sensor).
+   *
+   * @return value of some boolean subsystem state, such as a digital sensor.
+   */
+  public boolean exampleCondition() {
+    // Query some boolean state, such as a digital sensor.
+    return false;
+  }
+
+  @Override
+  public void periodic() {
+  
+  }
+
+  @Override
+  public void simulationPeriodic() {
+    // This method will be called once per scheduler run during simulation
+  }
+}
